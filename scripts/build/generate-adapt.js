118be18ee1e05570d50c1166049891b2342f7b0e
Kevin Gibbons <bakkot@gmail.com>
2018-04-03 13:26:21 -0700

    Add adapt (#39)
    * Add wrap
    
    * swap argument order
    
    * rename to adapt

---
+++ scripts/build/generate-adapt.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2018 Shape Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const spec = require('shift-spec').default;
+
+let content = `/**
+ * Copyright 2018 Shape Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as Shift from 'shift-ast';
+
+export default (fn, reducer) => ({
+  __proto__: reducer,
+${Object.keys(spec).map(typeName => `
+  reduce${typeName}(node, data) {
+    return fn(super.reduce${typeName}(node, data), node);
+  },`).join('\n')}
+});
+`;
+
+require('fs').writeFile('gen/adapt.js', content, 'utf-8');
---
+++ test/adapt.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018 Shape Security, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const assert = require('assert');
+
+const { reduce, MonoidalReducer, adapt } = require('../');
+const { parseModule } = require('shift-parser');
+
+suite('adapt', () => {
+  test('adapt adapts', () => {
+    const plusReducer = new MonoidalReducer({
+      empty() {
+        return 0;
+      },
+      concat(other) {
+        return this + other;
+      },
+    });
+    const countNodes = adapt(d => d + 1, plusReducer);
+
+    assert.equal(reduce(countNodes, parseModule('a + b')), 5);
+  });
+
+  test('adapt has node available to it', () => {
+    const concatReducer = new MonoidalReducer({
+      empty() {
+        return [];
+      },
+      concat(other) {
+        return this.concat(other);
+      },
+    });
+    const listNodes = adapt((d, node) => d.concat([node.type]), concatReducer);
+
+    assert.deepStrictEqual(reduce(listNodes, parseModule('a + b')), [
+      'IdentifierExpression',
+      'IdentifierExpression',
+      'BinaryExpression',
+      'ExpressionStatement',
+      'Module',
+    ]);
+  });
+});
