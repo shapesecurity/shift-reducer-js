b1dfabb3406fa0db3c14332228f2cb6848fe48d4
Kevin Gibbons <bakkot@gmail.com>
2018-05-07 23:19:43 -0700

    Add makeHeader helper

--- scripts/build/generate-lazy-clone-reducer.js
+++ scripts/build/generate-lazy-clone-reducer.js
@@ -17,7 +17,7 @@
 'use strict';
 
 const spec = require('shift-spec').default;
-const { sanitize, parameterize, isStatefulType, isNodeOrUnionOfNodes } = require('../lib/utilities.js');
+const { makeHeader, sanitize, parameterize, isStatefulType, isNodeOrUnionOfNodes } = require('../lib/utilities.js');
 
 function generateEquals(type, a, b) {
   switch (type.typeName) {
@@ -59,22 +59,7 @@ function generateEquals(type, a, b) {
   }
 }
 
-let content = `/**
- * Copyright 2017 Shape Security, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License")
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+let content = `${makeHeader(__filename)}
 import * as Shift from 'shift-ast';
 
 export default class LazyCloneReducer {`;
